caeb3cb6d0b741710e2a24048d22a61409478c07
Allison Byrnes <allison.byrnes42@gmail.com>
2026-06-09 22:38:41 -0700

    Fix graphics drivers

--- hosts/morpheus/default.nix
+++ hosts/morpheus/default.nix
@@ -10,11 +10,9 @@
     enable32Bit = true;
     extraPackages = with pkgs; [ 
         rocmPackages.clr.icd 
-        amdvlk
         mesa.drivers
     ];
     extraPackages32 = with pkgs; [
-        driversi686Linux.amdvlk
         pkgsi686Linux.mesa.drivers
     ];
   };
